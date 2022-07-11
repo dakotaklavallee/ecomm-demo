aca0eb4a26c7886884a6070d12cccb4f5c33232d
Dakota Lavallee <dakotalavallee@Dakotas-MBP.localdomain>
2022-07-11 14:42:08 -0500

    First Try

---
+++ ecom-backend/src/server.js
@@ -0,0 +1,21 @@
+const { PORT = 8080 } = process.env;
+
+const app = require("./app");
+const knex = require("./db/connection");
+
+app.listen(PORT, listener);
+
+// knex.migrate
+//   .latest()
+//   .then((migrations) => {
+//     console.log("migrations", migrations);
+//     app.listen(PORT, listener);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//     knex.destroy();
+//   });
+
+function listener() {
+  console.log(`Listening on Port ${PORT}!`);
+}
